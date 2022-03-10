b055c718571b71f06f53974a4916ee6e9d91f8a3
andyrude <anirudh.mishra_cs19@gla.ac.in>
2022-03-10 22:05:35 +0530

    LinkedListImpl

---
+++ src/linkedList/Main.java
@@ -0,0 +1,16 @@
+package linkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedListImpl list = new LinkedListImpl();
+        list.createNodes();
+        list.display();
+        System.out.println();
+        System.out.println("After Insertion at front");
+        list.addNodeAtFront(22);
+        list.display();
+        list.deleteAtFront();
+        System.out.println();
+        list.display();
+    }
+}

cca73ded29ea06b161c987de546abc6734203bf0
andyrude <anirudh.mishra_cs19@gla.ac.in>
2022-03-11 00:31:02 +0530

    Search In BST

---
+++ src/trees/binarysearchTree/SearchInBST.java
@@ -0,0 +1,24 @@
+package trees.binarysearchTree;
+
+import trees.TreeNode;
+
+public class SearchInBST {
+    public TreeNode searchBST(TreeNode root, int val) {
+
+        if(root==null){
+            return null;
+        }
+
+        if(root.val==val){
+            return root;
+        }
+
+        else if(root.val>val){
+            return searchBST(root.left, val);
+        }else{
+            return searchBST(root.right,val);
+        }
+
+    }
+}
+

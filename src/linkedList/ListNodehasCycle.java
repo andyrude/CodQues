b694fcc5a2f6c74d64f2eb763915c50aaf9ebdf5
andyrude <anirudh.mishra_cs19@gla.ac.in>
2022-03-10 22:17:11 +0530

    LinkedListImpl

--- src/linkedList/ListNodehasCycle.java
+++ src/linkedList/ListNodehasCycle.java
@@ -4,6 +4,9 @@ import linkedList.node.ListNode;
 
 public class ListNodehasCycle {
     public boolean hasCylce(ListNode head){
+        if ( head == null){
+            return false;
+        }
         ListNode fast = head;
         ListNode slow = head;
         while ( fast.next != null && fast.next.next != null){

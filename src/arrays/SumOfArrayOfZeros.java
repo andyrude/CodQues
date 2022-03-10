326780db526d99ebd9efc802ccafc27216cee003
andyrude <anirudh.mishra_cs19@gla.ac.in>
2022-03-11 01:37:31 +0530

    MinimumAbsoluteDifference

---
+++ src/arrays/SumOfArrayOfZeros.java
@@ -0,0 +1,25 @@
+package arrays;
+
+public class SumOfArrayOfZeros {
+    public static void main(String[] args) {
+        int[] arr = {0,0,0,6,0,0};
+        System.out.println( find( arr));
+    }
+
+    private static int find(int[] arr) {
+        int count = 0;
+        int c = 0;
+        int check = 0;
+        for ( int i = 0; i < arr.length; i++){
+            if ( arr[i] == 0){
+                c++;
+                count = count + c;
+                check = check + count;
+            }else {
+                c = 0;
+                count = 0;
+            }
+        }
+        return check;
+    }
+}

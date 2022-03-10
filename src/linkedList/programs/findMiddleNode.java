c073a86b71850534a8e653254aa7de17edf60df0
andyrude <anirudh.mishra_cs19@gla.ac.in>
2022-03-10 22:25:19 +0530

    LinkedListImpl

--- src/linkedList/programs/findMiddleNode.java
+++ src/linkedList/programs/findMiddleNode.java
@@ -4,13 +4,16 @@ import linkedList.Node;
 import linkedList.node.ListNode;
 
 public class findMiddleNode {
-    public int middleNode(ListNode head){
+    public ListNode middleNode(ListNode head){
         ListNode fast = head;
         ListNode slow = head;
         while ( fast.next != null && fast.next.next != null){
             slow = slow.next;
             fast = fast.next.next;
         }
-        return slow.val;
+        if(fast.next!=null){
+            return slow.next;
+        }
+        return slow;
     }
 }

e559f32487381e49ab36c42efa4c4a57df3f672c
andyrude <anirudh.mishra_cs19@gla.ac.in>
2022-03-11 21:52:04 +0530

    FibonacciUsingRecursion

--- src/arrays/FibonacciUsingRecursion.java
+++ src/arrays/FibonacciUsingRecursion.java
@@ -7,22 +7,20 @@ public class FibonacciUsingRecursion {
     public static void main(String[] args) {
         Scanner sc = new Scanner( System.in);
         int n = sc.nextInt();
-        int[] dp = new int[n + 1];
-        Arrays.fill( dp, -1);
-        dp[0] = 1;
-        dp[1] = 1;
-        System.out.println(getFibonacciSeq( n, dp));
-        System.out.println(Arrays.toString(dp));
+
+        System.out.println(getFibonacciSeq( n));;
     }
 
-    private static int getFibonacciSeq(int n, int[] dp) {
+    private static int getFibonacciSeq(int n) {
         if ( n == 0 || n == 1){
             return 1;
         }
+        int[] dp = new int[n + 1];
         if ( dp[n ] != -1){
             return dp[n ];
         }
-        dp[n ] = getFibonacciSeq( n - 1, dp) + getFibonacciSeq( n - 2, dp);
+        dp[n ] = getFibonacciSeq( n - 1) + getFibonacciSeq( n - 2);
+        System.out.println(Arrays.toString(dp));
         return dp[n ];
     }
 }

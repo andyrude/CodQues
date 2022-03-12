5fb29b2f18719f5176894d98c2a5bd6b12828e0e
andyrude <anirudh.mishra_cs19@gla.ac.in>
2022-03-13 01:18:26 +0530

    trees

--- src/trees/programs/FlipEquiTrees.java
+++ src/trees/programs/FlipEquiTrees.java
@@ -22,6 +22,9 @@ public class FlipEquiTrees {
                 if ( node1 == null || node2 == null){
                     return false;
                 }
+                if ( node1.data != node2.data){
+                    return false;
+                }
                 boolean noFlip = FlipEquivalentTrees( node1.left, node2.left) && FlipEquivalentTrees( node1.right, node2.right);
                 boolean Flip = FlipEquivalentTrees( node1.left, node2.right) && FlipEquivalentTrees( node1.right, node2.left);
                 return noFlip || Flip;

e20c3e5083d41fbbf953925e05b7b71cc9208964
andyrude <anirudh.mishra_cs19@gla.ac.in>
2022-03-13 19:33:07 +0530

    trees

---
+++ src/printingNumbersAfterCertainTime/PrintTime.java
@@ -0,0 +1,31 @@
+package printingNumbersAfterCertainTime;
+
+import java.util.Scanner;
+class PrintTime implements Runnable
+{
+    public void run()
+    {
+        try
+        {
+            for(int i=1;i<=10;i++)
+            {
+                Thread.sleep(2);
+                System.out.println(i);
+            }
+        }
+        catch(InterruptedException e)
+        {
+            System.out.println("Exception..."+e);
+        }
+    }
+}
+
+class Number
+{
+    public static void main(String [] args)
+    {
+        PrintTime m1 = new PrintTime();
+        Thread thread = new Thread(m1);
+        thread.start();
+    }
+}

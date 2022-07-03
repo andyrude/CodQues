358b6404d6c1a7672b6f5b968d372ea97f0e3070
andyrude <anirudh.mishra_cs19@gla.ac.in>
2022-07-04 00:21:44 +0530

    Minimum Operations

--- src/dynamicprogramming/ClimbingStaircaseUsingDP.java
+++ src/dynamicprogramming/ClimbingStaircaseUsingDP.java
@@ -13,7 +13,7 @@ public class ClimbingStaircaseUsingDP {
         int[] dp = new int[n+1];
         dp[n] = 1;
         for(int i=n-1; i>=0; i--){
-            for(int j=1; j<=arr[i] && i + j< dp.length; j++){
+            for(int j=1; j<=arr[i] && i + j < dp.length; j++){
                 dp[i] += dp[i+j];
             }
         }
